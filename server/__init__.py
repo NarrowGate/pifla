d654d2642dfdd26e145c7ebfe85ff9728cfa9288
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2020-02-22 21:37:03 +1000

    Add all pages

--- server/__init__.py
+++ server/__init__.py
@@ -3,10 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
-    return render_template('home.html', name="sdfsd")
+    return render_template('home.html')
 
 
-@app.route('/about')    
-def about():
-    return 'This is a about'
+@app.route('/contacts')    
+def contacts():
+    return render_template('contacts.html')
+
+
+@app.route('/todolist')    
+def todolist():
+    return render_template('to-do-list.html')
+
+@app.route('/spa')    
+def spa():
+    return render_template('spa.html')
